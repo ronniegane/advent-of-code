f8d2ed5d6397fdb0759ce93fe33fb85b79fb54b6
Ronnie Gane <dev@ronniegane.kiwi>
2020-12-07 12:23:30 +1300

    Using map instead of 2D list for tracing wires

--- 2019/3-crossed-wires/a/cross-wires.py
+++ 2019/3-crossed-wires/a/cross-wires.py
@@ -5,6 +5,13 @@
 # Draw second wire and check every step for an overlap
 # Keep track of running minimum
 
+# Issue with expanding grid - the circuit doesn't seem restricted to only grow right and up from [0,0].
+# Example test2 is R75, D30 so should end up at (75,-30)
+# But we can't use negative grid indices
+# Solution: use a map instead, with keys being x,y coords
+# This means it's quite a bit harder to pretty-print a grid for visual feedback,
+# but that isn't necessary to solve the problem.
+
 # Second idea: convert directions to a series of nodes
 # eg R8 U5 L5 D3 becomes (0,0) (8,0) (8,5) (3,5) (3,2)
 # Then check for crossover with each step in the second one
@@ -22,10 +29,11 @@ def pretty_print(grid: List[List[int]]):
 # test2 distance 159
 # test3 distance 135
 
-path = (Path(__file__).parent / "./test").resolve()
+
+path = (Path(__file__).parent / "./test2").resolve()
 with open(path, 'r') as fp:
     first_commands = fp.readline().split(',')
-    grid = [[0]]
+    grid = {}
     current_x = 0
     current_y = 0
     # Grid limits
@@ -36,34 +44,25 @@ with open(path, 'r') as fp:
     for command in first_commands:
         direction = command[0]
         distance = int(command[1:])
-        print('Current position is (%d, %d)' % (current_x, current_y))
-        print(command)
+        # print('Current position is (%d, %d)' % (current_x, current_y))
+        # print(command)
         if direction == 'L':
             for x in range(current_x-distance, current_x):
-                grid[x][current_y] = 1
+                grid["%d,%d" % (x, current_y)] = 1
             current_x = current_x - distance
         elif direction == 'R':
             for x in range(current_x, current_x + distance + 1):
-                # Expand grid if necessary
-                if x > right_edge:
-                    grid.append([0 for y in grid[0]])
-                    right_edge += 1
-                grid[x][current_y] = 1
+                grid["%d,%d" % (x, current_y)] = 1
             current_x = current_x + distance
         elif direction == 'U':
             for y in range(current_y, current_y + distance + 1):
-                # Expand grid if necessary
-                if y > top_edge:
-                    for col in grid:
-                        col.append(0)
-                    top_edge += 1
-                grid[current_x][y] = 1
+                grid["%d,%d" % (current_x, y)] = 1
             current_y = current_y + distance
         elif direction == 'D':
             for y in range(current_y - distance, current_y):
-                grid[current_x][y] = 1
+                grid["%d,%d" % (current_x, y)] = 1
             current_y = current_y - distance
-        pretty_print(grid)
+        # pretty_print(grid)
 
     # Draw second circuit
     second_commands = fp.readline().split(',')
@@ -73,70 +72,60 @@ with open(path, 'r') as fp:
     min_coords = [0, 0]
 
     # Reset the central port so we don't mark it as a crossover
-    grid[0][0] = 8
+    grid['0,0'] = 8
 
     print('\nDrawing second circuit:')
     # At each step check for overlaps and keep track of the closest overlap seen so far
     for command in second_commands:
         direction = command[0]
         distance = int(command[1:])
-        print('Current position is (%d, %d)' % (current_x, current_y))
-
-        print(command)
+        # print('Current position is (%d, %d)' % (current_x, current_y))
+        # print(command)
         if direction == 'L':
             for x in range(current_x-distance, current_x):
-                if grid[x][current_y] == 1:
-                    grid[x][current_y] = 3
+                if "%d,%d" % (x, current_y) not in grid:
+                    grid["%d,%d" % (x, current_y)] = 2
+                elif grid["%d,%d" % (x, current_y)] == 1:
+                    grid["%d,%d" % (x, current_y)] = 3
                     dist_to_home = x + current_y
                     if dist_to_home < min_dist:
                         min_dist = dist_to_home
                         min_coords = [x, current_y]
-                elif grid[x][current_y] == 0:
-                    grid[x][current_y] = 2
             current_x = current_x - distance
         elif direction == 'R':
             for x in range(current_x, current_x + distance + 1):
-                # Expand grid if necessary
-                if x > right_edge:
-                    grid.append([0 for y in grid[0]])
-                    right_edge += 1
-                if grid[x][current_y] == 1:
-                    grid[x][current_y] = 3
+                if "%d,%d" % (x, current_y) not in grid:
+                    grid["%d,%d" % (x, current_y)] = 2
+                elif grid["%d,%d" % (x, current_y)] == 1:
+                    grid["%d,%d" % (x, current_y)] = 3
                     dist_to_home = x + current_y
                     if dist_to_home < min_dist:
                         min_dist = dist_to_home
                         min_coords = [x, current_y]
-                elif grid[x][current_y] == 0:
-                    grid[x][current_y] = 2
             current_x = current_x + distance
         elif direction == 'U':
             for y in range(current_y, current_y + distance + 1):
-                # Expand grid if necessary
-                if y > top_edge:
-                    for col in grid:
-                        col.append(0)
-                    top_edge += 1
-                if grid[current_x][y] == 1:
-                    grid[current_x][y] = 3
+                if "%d,%d" % (current_x, y) not in grid:
+                    grid["%d,%d" % (current_x, y)] = 2
+                elif grid["%d,%d" % (current_x, y)] == 1:
+                    grid["%d,%d" % (current_x, y)] = 3
                     dist_to_home = current_x + y
                     if dist_to_home < min_dist:
                         min_dist = dist_to_home
                         min_coords = [current_x, y]
-                elif grid[current_x][y] == 0:
-                    grid[current_x][y] = 2
             current_y = current_y + distance
         elif direction == 'D':
             for y in range(current_y - distance, current_y):
-                if grid[current_x][y] == 1:
-                    grid[current_x][y] = 3
+                if "%d,%d" % (current_x, y) not in grid:
+                    grid["%d,%d" % (current_x, y)] = 2
+                elif grid["%d,%d" % (current_x, y)] == 1:
+                    grid["%d,%d" % (current_x, y)] = 3
                     dist_to_home = current_x + y
                     if dist_to_home < min_dist:
                         min_dist = dist_to_home
                         min_coords = [current_x, y]
-                elif grid[current_x][y] == 0:
-                    grid[current_x][y] = 2
             current_y = current_y - distance
-        pretty_print(grid)
+        # pretty_print(grid)
 
     print('Crossover at (%d, %d) distance of %d' %
           (min_coords[0], min_coords[1], min_dist))
