1473bf58923a627d25207568799d47d57eb701ae
Ronnie Gane <dev@ronniegane.kiwi>
2020-12-29 13:12:37 +1300

    2020 Day 13 part two - added more test cases

--- 2020/13-shuttle-search/b/consecutive-buses.py
+++ 2020/13-shuttle-search/b/consecutive-buses.py
@@ -2,6 +2,7 @@
 We want each bus to arrive at time T + i
 where i is its index in the list of buses (including x)
 
+The question indicates that the solution is going to be > 100000000000000 (10^14)
 Brute-force solution is going to be terribly slow,
 so we need to find an algorithm or a formulaic solution
 """
@@ -9,7 +10,12 @@ so we need to find an algorithm or a formulaic solution
 from pathlib import Path
 import math
 
-# Test: first occurs at 1068781
+# Test1: first occurs at 1068781
+# Test2: first occurs at 3417
+# Test3: first occurs at 754018
+# Test4: first occurs at 779210
+# Test5: first occurs at 1261476
+# Test6: first occurs at 1202161486
 path = (Path(__file__).parent / '../input').resolve()
 fp = open(path, 'r')
 # First line is the earliest time _we_ can leave
